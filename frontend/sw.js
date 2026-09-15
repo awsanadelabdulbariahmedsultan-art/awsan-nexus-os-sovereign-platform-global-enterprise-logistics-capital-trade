9660de8ff203f189860e0f4b6b3c818d4bcf9a65
Awsan Adel Abdulbari Ahmed Sultan <awsan.sultan@gmail.com>
2026-09-15 22:04:31 +0300

    Add service worker for caching assets

---
+++ frontend/sw.js
@@ -0,0 +1,26 @@
+const CACHE_NAME = 'nexus-v2.5';
+const ASSETS_TO_CACHE = ['/', '/manifest.json'];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS_TO_CACHE))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      return cachedResponse || fetch(event.request);
+    })
+  );
+});
